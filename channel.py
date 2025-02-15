b4dc643d92594ffb3536a04e7114a58ae4ca3b97
abrech <adrian.brechtken@gmail.com>
2025-02-15 17:16:10 +0100

    timeformat

--- channel.py
+++ channel.py
@@ -107,10 +107,6 @@ def send_message():
         return "No sender", 400
     if not 'timestamp' in message:
         return "No timestamp", 400
-    else:
-        pattern = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}Z$"
-        if not (bool(re.match(pattern, message['timestamp']))):
-            return "Invalid timestamp", 400
 
     if not 'extra' in message:
         extra = None
